2d086851a024eb134203c8e231b56cedfbf6a65f
Yaser <ahmadyaserazizi2001@gmail.com>
2022-07-13 22:49:57 -0700

    profile

---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Budgets App',
+    description: "Using React & Bootstrap you'll learn how to build a Budgets Application - from start to finish.",
+      image: '/images/1.png',
+      tags: ['React', 'Bootstrap'],
+    source: 'https://github.com/ahmadyaser2001/budget-app',
+    visit: 'https://budget-app-mocha-delta.vercel.app/',
+    id: 0,
+  },
+  {
+    title: 'Hulu Web',
+    description:"While building it you're going to learn many advanced JavaScript topics, you will be able to add to your portfolio. And trust me, Hulu applications are impressive.",
+    image: '/images/2.png',
+    tags: ['Html', 'css'],
+    source: 'https://github.com/ahmadyaser2001/hulu-webpage',
+    visit: 'https://hulu-webpage-zeta.vercel.app/',
+    id: 1,
+  },
+  {
+    title: 'NFT App',
+    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a NFT WEB.",
+      image: '/images/3.png',
+      tags: ['React', 'Tialwind css'],
+    source: 'https://github.com/ahmadyaser2001/NFT',
+    visit: 'https://nft-nine-ochre.vercel.app/',
+    id: 2,
+  },
+  {
+    title: 'Wheater App',
+    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a Wheater App",
+    image: '/images/4.png',
+    tags: ['React', 'Css', 'API'],
+    source: 'https://github.com/ahmadyaser2001/weather-app',
+    visit: 'https://weather-app-blond-alpha.vercel.app/',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2018, text: 'Started my journey', },
+  { year: 2019, text: 'Worked as a freelance developer', },
+  { year: 2020, text: 'Founded Ahmad Yaser', },
+  { year: 2021, text: 'Shared my projects with the world', },
+  { year: 2022, text: 'Started my own platform', },
+];
